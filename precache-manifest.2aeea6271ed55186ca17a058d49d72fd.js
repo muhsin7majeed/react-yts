897f38d4eff578335c6e5f9e02f50d75c09e0aca
Muhsin <muhsin7ak@gmail.com>
2019-06-24 13:49:11 +0530

    Updates

---
+++ precache-manifest.2aeea6271ed55186ca17a058d49d72fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a0b1004df979a9268a100a82ec2540",
+    "url": "/react-yts/index.html"
+  },
+  {
+    "revision": "cee319569570cb392d0e",
+    "url": "/react-yts/static/css/main.62f842d3.chunk.css"
+  },
+  {
+    "revision": "146906bbc55ccde847cd",
+    "url": "/react-yts/static/js/2.98d18a7b.chunk.js"
+  },
+  {
+    "revision": "cee319569570cb392d0e",
+    "url": "/react-yts/static/js/main.c828950f.chunk.js"
+  },
+  {
+    "revision": "d6be4d705e92a8d2856a",
+    "url": "/react-yts/static/js/runtime~main.b591636e.js"
+  }
+]);
